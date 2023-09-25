579d8099887f5ea25dc8e5c1c46eb746f41d4994
student <student@gnu.ac.in>
2023-09-25 10:47:15 +0530

    completed

---
+++ app/src/main/java/com/example/mad_practical8_21012011118/AlarmBroadcastReceiver.kt
@@ -0,0 +1,28 @@
+package com.example.mad_practical8_21012011118
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+
+    companion object{
+        val ALARM_KEY = "KEY"
+        val ALARM_STOP = "STOP"
+        val ALARM_START = "START"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        val data = intent.getStringExtra(ALARM_KEY)
+        val intentService = Intent(context, AlarmService::class.java)
+        if(data == ALARM_START)
+        {
+            context.startService(intentService)
+        }
+        else if(data == ALARM_STOP)
+        {
+            context.stopService(intentService)
+        }
+    }
+}
--- app/src/main/java/com/example/mad_practical8_21012011118/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical8_21012011118/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.mad_practical8_21012011118
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val createAlramBtn:Button = findViewById(R.id.createAlarmbutton)
+        val cardView2: MaterialCardView = findViewById(R.id.cardView2)
+        val cancelAlarmBtn:Button = findViewById(R.id.cancelAlarmbutton)
+
+        createAlramBtn.setOnClickListener {
+//            if(cardView2.visibility==View.GONE)
+//            {
+//                cardView2.visibility = View.VISIBLE
+//            }
+            cardView2.visibility = View.VISIBLE
+        }
+
+        cancelAlarmBtn.setOnClickListener {
+//            if(cardView2.visibility==View.VISIBLE)
+//            {
+//                cardView2.visibility = View.GONE
+//            }
+            cardView2.visibility = View.GONE
+        }
+    }
+
+    fun setAlarm(milliTime:Long, action:String)
+    {
+        val intentAlarm = Intent(applicationContext, AlarmBroadcastReceiver::class.java)
+        intentAlarm.putExtra(AlarmBroadcastReceiver.ALARM_KEY, action)
+        val pendingIntent = PendingIntent.getBroadcast(applicationContext, 2345, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT)
+        val manager = getSystemService(ALARM_SERVICE) as AlarmManager
+        if(action == AlarmBroadcastReceiver.ALARM_START)
+        {
+            manager.setExact(AlarmManager.RTC_WAKEUP, milliTime, pendingIntent)
+        }
     }
 }
